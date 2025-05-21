0ed58fdeb877c7a1a144e229677118e31e93fe78
Gabriel Prata <ghprata7245@gmail.com>
2025-05-20 22:37:35 -0300

    Query list by id

--- src/main/java/com/prata/dslist/services/GameService.java
+++ src/main/java/com/prata/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.prata.dslist.services;
 import com.prata.dslist.dto.GameDTO;
 import com.prata.dslist.dto.GameMinDTO;
 import com.prata.dslist.entities.Game;
+import com.prata.dslist.projections.GameMinProjection;
 import com.prata.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> fingAll(){
        List<Game> result = gameRepository.findAll();
+        return  result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> fingByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return  result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
